268481f6a33d9721ed4289c72e311659b17fa293
mdmohid <alammohid855@gmail.com>
2024-02-28 20:25:31 +0545

    PersonalDetails.java

---
+++ week_1/PersonalDetails.java
@@ -0,0 +1,9 @@
+public class PersonalDetails {
+    public static void main(String[] args){
+        System.out.println("Name: Md. Mohid Alam");
+        System.out.println("Address: Kathmandu");
+        System.out.println("Age: 21");
+        System.out.println("Phone number: +9779819965626");
+    }
+      
+}
