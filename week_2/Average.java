357f69ca91d619f7f18e830e98856706eedc710b
mdmohid <alammohid855@gmail.com>
2024-03-03 15:20:22 +0545

    Average.java

---
+++ week_2/Average.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Average
+{
+    public static void main(String[] args)
+    {
+       int val1, val2, val3;
+       double average;
+       Scanner scan = new Scanner(System.in) ;
+
+       // get three values from user
+       System.out.println("Please enter three integers and " +
+                       "I will compute their average");
+       System.out.println("Enter the first value:");
+       val1=scan.nextInt();
+       System.out.println("Enter the second value:");
+       val2=scan.nextInt();
+       System.out.println("Enter the third value:");
+       val3=scan.nextInt();
+       //compute the average
+       average=val1+val2+val3;
+       //print the average
+       System.out.println("The average is " +average+".");
+    }
+}
