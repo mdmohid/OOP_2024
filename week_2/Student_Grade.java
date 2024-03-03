5d9933231778d9e2dbc1f3ce0bd94401e592e20a
mdmohid <alammohid855@gmail.com>
2024-03-03 15:18:39 +0545

    Student_Grade.java

---
+++ week_2/Student_Grade.java
@@ -0,0 +1,16 @@
+public class Student_Grade {
+    public static void main(String[] args) {
+        System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("==          Student Points          ==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\////////////////////");
+        System.out.println("Name        Lab       Bonus      Total");
+        System.out.println("----        ---       -----      -----");
+        System.out.println("Joe          43        7          50");
+        System.out.println("William      50        8          58");
+        System.out.println("Mary Sue     39        10         49");
+        System.out.println("MD           55        10         65");
+        System.out.println("Ali          45        8          53");
+    }
+    
+}
+
